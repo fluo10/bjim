710d4f7c4db39bdaba62c86cb78da36e14c3bab9
fluo10 <fluo10.dev@fireturtle.net>
2022-01-29 09:40:29 +0900

    Fix base path of walkdir

--- lib/src/data/mod.rs
+++ lib/src/data/mod.rs
@@ -14,13 +14,13 @@ impl Data {
     pub fn new(path: impl AsRef<Path>) -> Result<Self, String> {
         let mut data = Self {
             data_dir: path.as_ref().to_path_buf(),
-            pages: HashMap::with_capacity(4),
+            pages: HashMap::new(),
         };
         data.reload();
         Ok(data)
     }
     pub fn reload(&mut self) {
-        for entry in walkdir::WalkDir::new(".").into_iter().filter_map(|e| {
+        for entry in walkdir::WalkDir::new(self.data_dir.to_path_buf()).into_iter().filter_map(|e| {
             if e.as_ref().unwrap().path().extension()?.to_str().unwrap() == "md" {
                 e.ok()
             } else {
