1c324efb31d70c0a221495e938c59df9accaf7c9
fluo10 <fluo10.dev@fireturtle.net>
2022-02-01 08:56:53 +0900

    Add regex and test for task status

--- lib/src/page/content/mod.rs
+++ lib/src/page/content/mod.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use super::bullet::TaskStatus;
 
 pub struct PageContent {
@@ -10,8 +11,11 @@ impl PageContent {
             raw: String::from(raw),
         }
     }
-    pub fn replace_task_status(&mut self, from: TaskStatus, to: TaskStatus) {
-
+    pub fn replace_task_status(&mut self, before: TaskStatus, after: TaskStatus) {
+        let pattern = format!(r"(?m)^(- \[)[{}](\] .*)$", before);
+        let replacement = format!("$1{}$2", after);
+        let re = Regex::new(pattern.as_str()).unwrap();
+        self.raw = re.replace_all(self.raw.as_str(), replacement.as_str()).to_string();
     }
     pub fn filter_open_tasks(&mut self) {
 
@@ -19,3 +23,24 @@ impl PageContent {
     
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn replace_task_status() {
+        let mut before = PageContent::from_str(r"
+- [ ] open-task
+- [>] Migrated task
+- [<] Scheduled task
+- [/] Task in progress
+- [x] Closed task");
+        before.replace_task_status(TaskStatus::Open, TaskStatus::Scheduled);
+        assert_eq!(before.raw, String::from(r"
+- [x] open-task
+- [>] Migrated task
+- [<] Scheduled task
+- [/] Task in progress
+- [x] Closed task"));
+    }
+
+}
