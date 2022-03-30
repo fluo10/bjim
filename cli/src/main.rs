0286b43d58be7e3805be5a4785d7d2d2718cf322
fluo10 <fluo10.dev@fireturtle.net>
2022-03-30 12:41:26 +0900

    Replace print to log about migrate

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,9 +2,13 @@ mod cmd;
 mod args;
 use cmd::Cmd;
 
+use env_logger::{Builder, Env};
 use clap::Parser;
 
 fn main() {
+    Builder::from_env(Env::default().default_filter_or("info"))
+        .format_timestamp(None)
+        .init();
     let cmd = Cmd::parse();
     cmd.run();
 }
